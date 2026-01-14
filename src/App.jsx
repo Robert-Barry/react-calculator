efad7e700f94a6cd81ff1a3d1839940d318fb236
Robert Barry <rb4080@gmail.com>
2026-01-14 11:54:22 -0500

    Add basic elements for a graphical calulator.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,41 @@ function App() {
   return (
     <>
       <h1>React Calculator</h1>
+      <div id='calculator'>
+        <div className='row'>
+          <input type='number' id='output' name='output' readOnly />
+        </div>
+        <div className='row'>
+          <button>&lt;</button>
+          <button>AC</button>
+          <button>%</button>
+          <button>/</button>
+        </div>
+        <div className='row'>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+        </div>
+        <div className='row'>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div className='row'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        <div className='row'>
+          <button>+/-</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </>
   )
 }
